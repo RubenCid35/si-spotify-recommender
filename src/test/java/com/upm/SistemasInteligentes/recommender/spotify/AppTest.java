813e97dc3616e2106e03c005ab2c67fc97699b75
RubenCid35 <rubencid001@gmail.com>
2023-05-23 19:50:31 +0200

    basic maven project

---
+++ src/test/java/com/upm/SistemasInteligentes/recommender/spotify/AppTest.java
@@ -0,0 +1,20 @@
+package com.upm.SistemasInteligentes.recommender.spotify;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
